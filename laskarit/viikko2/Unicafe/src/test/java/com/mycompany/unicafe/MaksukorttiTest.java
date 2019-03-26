dbff8da69dac4bd484910baa0bb3b895c356dd81
Touko Hanhijoki <toukkeli@hotmail.com>
2019-03-26 15:38:18 +0200

    Laskarien jatkoa

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,40 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void kortinSaldoOnAlussaOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());     
+    }
+    
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(10);
+        assertEquals("saldo: 0.20", kortti.toString()); 
+    }
+    
+    @Test
+    public void saldoVaheneeOikeinJosRahaaOnTarpeeksi() {
+        kortti.otaRahaa(5);
+        assertEquals("saldo: 0.05", kortti.toString());      
+    }
+    
+    @Test
+    public void saldoEiMuutuJosRahaaEiOleTarpeeksi() {
+        kortti.otaRahaa(15);
+        assertEquals("saldo: 0.10", kortti.toString());         
+    }
+    
+    @Test
+    public void onnistunutRahanOttaminenPalauttaaTrue() {
+        assertTrue(kortti.otaRahaa(5));      
+    }
+    
+    @Test
+    public void epaonnistunutRahanOttaminenPalauttaaFalse() {
+        assertTrue(!kortti.otaRahaa(15));      
+    }
+    
+    
+
 }
